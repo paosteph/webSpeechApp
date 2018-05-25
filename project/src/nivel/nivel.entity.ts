145870df4423a781d25f1e5179ad6013c3ee82ba
kersha38 <carlos.gutierrez@epn.edu.ec>
2018-05-25 17:25:32 -0500

    :v

--- project/src/nivel/nivel.entity.ts
+++ project/src/nivel/nivel.entity.ts
@@ -2,6 +2,7 @@ import {Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity('nivel')
 export class NivelEntity{
+
     @PrimaryGeneratedColumn()
     id: number
 }
--- project/src/usuario/usuario.entity.ts
+++ project/src/usuario/usuario.entity.ts
@@ -1,7 +1,7 @@
 import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 
 
-@Entity('Usuario')
+@Entity('usuario')
 export class UsuarioEntity {
 
     @PrimaryGeneratedColumn()
