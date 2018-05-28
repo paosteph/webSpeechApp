2202da896fe76b0cbf5b07a00f8b24441830de02
GracielaMoreno <chelinesmoreno@hotmail.com>
2018-05-27 19:56:42 -0500

    home html

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,13 +1,16 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-
+const fs = require('fs');
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   root(): string {
-    return this.appService.root();
+      console.log('Inicio al programa');
+      let datoArchivo;
+      let html = fs.readFileSync(__dirname + '/html/Index.html','utf8');
+      return  html;
   }
 
 
