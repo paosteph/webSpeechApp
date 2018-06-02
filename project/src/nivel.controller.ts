46b3711a0ac2b28391e1f927e52b3e704e44032c
kersha38 <carlos.gutierrez@epn.edu.ec>
2018-06-02 17:25:10 -0500

    correccion

--- project/src/nivel.controller.ts
+++ project/src/nivel.controller.ts
@@ -1,15 +1,11 @@
-import {Controller} from "@nestjs/common";
-import {Get, Post} from "@nestjs/common/utils/decorators/request-mapping.decorator";
-import {Body} from "@nestjs/common/utils/decorators/route-params.decorator";
+import {Controller, Get, Post, Body} from "@nestjs/common";
 import {UsuarioPipe} from "./usuario/usuario.pipe";
 import {Admin_Login_Schema} from "./Administrador/administrador.schema";
-import {Admin} from "typeorm";
+
 
 @Controller('nivel')
 export class NivelController{
-    constructor(){
 
-    }
     @Post('ingresar')
     ingresar(@Body(new UsuarioPipe(Admin_Login_Schema))administrador){
         return administrador;
