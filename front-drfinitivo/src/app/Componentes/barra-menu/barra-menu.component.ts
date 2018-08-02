57b64db27da8a88146dd5a1c5060915f6d218cf1
GracielaMoreno <chelinesmoreno@hotmail.com>
2018-08-02 00:03:37 -0500

    solo frond inicio componen

--- front-drfinitivo/src/app/Componentes/barra-menu/barra-menu.component.ts
+++ front-drfinitivo/src/app/Componentes/barra-menu/barra-menu.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class BarraMenuComponent implements OnInit {
 
   constructor() { }
-
+private botonMenu;
   ngOnInit() {
+    this.botonMenu="Menu";
   }
 
 }
